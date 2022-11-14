0ddabfdd478f75d1bd82824f92a42810f217a22e
Yujaebin <dbwpqls9998@naver.com>
2022-11-14 22:41:40 +0900

    Feat: react-router 추가

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import PostList from "./components/PostList";
-import TeachStack from "./components/TeachStack";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MainCarousel from "./components/MainCarousel";
-
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+// Router 관련페이지
+// https://velog.io/@velopert/react-router-v6-tutorial#35-link-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%EB%8B%A4%EB%A5%B8-%ED%8E%98%EC%9D%B4%EC%A7%80%EB%A1%9C-%EC%9D%B4%EB%8F%99%ED%95%98%EB%8A%94-%EB%A7%81%ED%81%AC-%EB%B3%B4%EC%97%AC%EC%A3%BC%EA%B8%B0
 function App() {
   return (
-    <React.Fragment>
-      <GlobalStyles
-        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
-      />
-      <CssBaseline />
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
 
-      {/* begin of header*/}
-      <Header />
-      {/* end header */}
-
-      {/* slide */}
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <MainCarousel></MainCarousel>
-        </Grid>
-        {/* slide */}
-
-        {/* TechStack */}
-        <Grid item xs={1.5}></Grid>
-        <Grid item xs={9}>
-          <TeachStack />
-        </Grid>
-        <Grid item xs={1.5}></Grid>
-        {/* TechStack */}
-
-        {/* PostList */}
-        <Grid item xs={1.5}></Grid>
-        <Grid item xs={9}>
-          <PostList />
-        </Grid>
-        <Grid item xs={1.5}></Grid>
-        {/* PostList */}
-      </Grid>
-      {/* Footer */}
-      <Footer />
-      {/* End footer */}
-    </React.Fragment>
+        {/* 나중에 404페이지를 넣어야합니다 ↓ */}
+        <Route path="*" element={<MainPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/MainPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import PostList from "../components/PostList";
+import TeachStack from "../components/TeachStack";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MainCarousel from "../components/MainCarousel";
+const MainPage = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
+      <CssBaseline />
+
+      {/* begin of header*/}
+      <Header />
+      {/* end header */}
+
+      {/* slide */}
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <MainCarousel></MainCarousel>
+        </Grid>
+        {/* slide */}
+
+        {/* TechStack */}
+        <Grid item xs={1.5}></Grid>
+        <Grid item xs={9}>
+          <TeachStack />
+        </Grid>
+        <Grid item xs={1.5}></Grid>
+        {/* TechStack */}
+
+        {/* PostList */}
+        <Grid item xs={1.5}></Grid>
+        <Grid item xs={9}>
+          <PostList />
+        </Grid>
+        <Grid item xs={1.5}></Grid>
+        {/* PostList */}
+      </Grid>
+      {/* Footer */}
+      <Footer />
+      {/* End footer */}
+    </React.Fragment>
+  );
+};
+
+export default MainPage;
