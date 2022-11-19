54e950f9f42b23dccb70d045b8944bc9be9f04b0
taeyee <taeyeeya@gmail.com>
2022-11-19 21:26:11 +0900

    Style :  내 정보수정 페이지 퍼블리싱

--- src/pages/InformationEdit.js
+++ src/pages/InformationEdit.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import UserImg from "../components/InformationEdit/UserImg";
+import UserName from "../components/InformationEdit/UserName";
+import UserStack from "./../components/InformationEdit/UserStack";
 // 내정보 수정 페이지
 
 const InformationEdit = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <h1>내 정보 수정</h1>
+      <UserImg />
+      <UserName />
+      <UserStack />
+      <button>완료</button>
+      <button>회원탈퇴</button>
+      <Footer />
+    </div>
+  );
 };
 
 export default InformationEdit;
