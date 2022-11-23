66158848634ca7193bb89f4ab41185b279b4f6b3
jiyeon22 <kkll4106@naver.com>
2022-11-23 12:41:00 +0900

    style : PostComent,PostView,Post.css UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
+import PostComent from "./components/ReadPage/PostComent";
+import PostView from "./components/ReadPage/PostView";
 import MainPage from "./pages/MainPage";
 // Router 관련페이지
 // https://velog.io/@velopert/react-router-v6-tutorial#35-link-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%EB%8B%A4%EB%A5%B8-%ED%8E%98%EC%9D%B4%EC%A7%80%EB%A1%9C-%EC%9D%B4%EB%8F%99%ED%95%98%EB%8A%94-%EB%A7%81%ED%81%AC-%EB%B3%B4%EC%97%AC%EC%A3%BC%EA%B8%B0
@@ -7,10 +9,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/" element={<PostView />}></Route>
 
         {/* 나중에 404페이지를 넣어야합니다 ↓ */}
-        <Route path="*" element={<MainPage />}></Route>
+        <Route path="*" element={<PostView />}></Route>
       </Routes>
     </>
   );
--- src/components/ReadPage/PostView.js
+++ src/components/ReadPage/PostView.js
@@ -1,7 +1,102 @@
+import { textAlign } from "@mui/system";
 import React from "react";
+import "./Post.css";
+import Header from "../Header";
+import Footer from "../Footer";
+import PostComent from "./PostComent";
+
 //게시물 작성 완료 페이지의 내용
 const PostView = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <div>
+        <h2 align="center">[서울대입구역]오프라인 스터디원 모집합니다.</h2>
+      </div>
+
+      <div id="content">
+        <div className="TextBox">
+          <div className="imagebox">
+            <img
+              class="profile"
+              src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTEwMThfMTI5%2FMDAxNjM0NTU2MDA4MDI3.zsBOZlr1ORTLG1JrR28FSt_UKlAbgTR2036EPOeSzfEg.9IXalR6AGH7G5i3j-Xjm1ht7-CFChgD2woGJDDWFN4Mg.JPEG.guskr0512%2FIMG_4659.JPG&type=sc960_832"
+            />
+          </div>
+        </div>
+        <div className="TextBox">이름</div>
+        <div className="TextBox">|</div>
+        <div className="TextBox">2022.11.21(게시글작성날짜)</div>
+      </div>
+
+      <div>
+        <hr></hr>
+        <ul className="studyGrid">
+          <li className="contentWrapper">
+            <span className="title">모집 구분</span>
+            <span className="title">프로젝트</span>
+            {/* <span className="content">{read.post.type.label}</span> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">진행 방식</span>
+            <span className="title">오프라인</span>
+            {/* <span className="content">{read.post.onlineOrOffline.label}</span> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">모집 인원</span>
+            <span className="title">인원 미정</span>
+            {/* <span className="content">{read.post.recruits.label}</span> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">시작 예정</span>
+            <span className="title">2022.12.01</span>
+            {/* <span className="content">{formatDate(read.post.startDate)}</span> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">연락 방법</span>
+            <span className="title">구글폼</span>
+            {/* <ContactPoint
+          contactPoint={read.post.contactPoint}
+          contactType={read.post.contactType}
+        /> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">예상 기간</span>
+            <span className="title">장기</span>
+            {/* <span className="content">{read.post.expectedPeriod.label}</span> */}
+          </li>
+          <li className="contentWrapper">
+            <span className="title">사용 언어</span>
+            {/* <ul className="languageList">
+          {read.post.language.map((lang, i) => (
+            <li key={i} className={styles.language}>
+              <img
+                className="languageImage"
+                src={`/images/languages/${lang.value}.svg`}
+                alt="language"
+              />
+            </li>
+          ))}
+        </ul> */}
+          </li>
+        </ul>
+      </div>
+
+      {/* 프로젝트 소개  */}
+
+      <div className="postContentWrapper">
+        <h2 className="postInfo">프로젝트 소개</h2>
+        <hr></hr>
+        <div
+          className="postContent"
+          // dangerouslySetInnerHTML={{ __html: read.post.content }}
+        >
+          게시글 내용
+        </div>
+        <PostComent />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default PostView;
