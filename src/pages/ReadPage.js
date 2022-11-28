3d8712c771317633337774757c3fb57e7b8c0ea4
jiyeon22 <kkll4106@naver.com>
2022-11-29 07:24:53 +0900

    feat: grid 조정

--- src/pages/ReadPage.js
+++ src/pages/ReadPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../components/Header";
+import { Grid } from "@mui/material";
+import PostView from "../components/ReadPage/PostView";
+
 //게시물 작성 완료 페이지 (작성한 글(1개)을 볼 수 있음)
 const ReadPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Grid container spacing={3}>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <PostView />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default ReadPage;
