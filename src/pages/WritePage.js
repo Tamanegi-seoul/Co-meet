97230fde42dc00571b25057895b3e1178d8474cb
jiyeon22 <kkll4106@naver.com>
2022-12-02 06:34:14 +0900

    feat: select 수정, 취소&글등록 버튼 추가

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, inputAdornmentClasses } from "@mui/material";
 import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
@@ -11,6 +11,7 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 // 게시물 작성 페이지
 const Title = styled.div`
   border-bottom: 1px solid;
@@ -29,11 +30,74 @@ const Circle = styled.div`
 `;
 
 const WritePage = () => {
-  const [age, setAge] = useState("");
+  const [recruit_status, setStatus] = useState(""); //모집구분
+  const [recruit_capacity, setCapacity] = useState(""); //모집인원
+  const [type, setType] = useState(""); //진행방식
+  const [expected_term, setTerm] = useState(""); //진행기간
+  const [stacklist, setStacklist] = useState(""); //기술스택
+
   const [startDate, setStartDate] = useState(new Date());
-  const handleChange = event => {
-    setAge(event.target.value);
+
+  const statusHandler = e => {
+    setStatus(e.target.value);
+    console.log(e.target.value);
   };
+
+  const capacityHandler = e => {
+    setCapacity(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const typeHandler = e => {
+    setType(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const termHandler = e => {
+    setTerm(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const stacklistHandler = e => {
+    setStacklist(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const addPost = e => {
+    console.log("글등록");
+  };
+
+  const back = e => {
+    console.log("취소");
+  };
+  // const handleChange = event => {
+  //   setCapacity(event.target.value);
+  // };
+
+  // //지연
+  // const [form, setForm] = useState({
+  //   status: "",
+  //   capacity: "",
+  //   type: "",
+  //   term: "",
+  //   stacklist: "",
+  // });
+
+  // const { status, capacity, type, term, stacklist } = form;
+
+  // const handleChange = e => {
+  //   console.log(e.target);
+  //   const { form, value } = e.target;
+  //   setForm({
+  //     ...form,
+  //     [form]: value,
+  //   });
+
+  //   setForm(form);
+  //   console.log(setForm);
+  // };
+
+  //
 
   return (
     <>
@@ -52,11 +116,18 @@ const WritePage = () => {
                 width: { md: "100%", lg: "47.9%" },
               }}
             >
-              <InputLabel>Age</InputLabel>
-              <Select value={age} label="Age" onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+              <InputLabel>모집구분</InputLabel>
+              <Select
+                value={recruit_status}
+                label="setForm"
+                onChange={statusHandler}
+              >
+                <MenuItem name="스터디" value={10}>
+                  스터디
+                </MenuItem>
+                <MenuItem name="프로젝트" value={20}>
+                  프로젝트
+                </MenuItem>
               </Select>
             </FormControl>
             <FormControl
@@ -66,11 +137,23 @@ const WritePage = () => {
                 width: { md: "100%", lg: "47.9%" },
               }}
             >
-              <InputLabel>Age</InputLabel>
-              <Select value={age} label="Age" onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+              <InputLabel>모집인원</InputLabel>
+              <Select
+                value={recruit_capacity}
+                label="form"
+                onChange={capacityHandler}
+              >
+                <MenuItem value={10}>인원미정</MenuItem>
+                <MenuItem value={20}>1명</MenuItem>
+                <MenuItem value={30}>2명</MenuItem>
+                <MenuItem value={40}>3명</MenuItem>
+                <MenuItem value={50}>4명</MenuItem>
+                <MenuItem value={60}>5명</MenuItem>
+                <MenuItem value={70}>6명</MenuItem>
+                <MenuItem value={80}>7명</MenuItem>
+                <MenuItem value={90}>8명</MenuItem>
+                <MenuItem value={100}>9명</MenuItem>
+                <MenuItem value={110}>10명이상</MenuItem>
               </Select>
             </FormControl>
             <FormControl
@@ -80,11 +163,10 @@ const WritePage = () => {
                 width: { md: "100%", lg: "47.9%" },
               }}
             >
-              <InputLabel>Age</InputLabel>
-              <Select value={age} label="Age" onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+              <InputLabel>진행방식</InputLabel>
+              <Select value={type} label="form_how" onChange={typeHandler}>
+                <MenuItem value={10}>온라인</MenuItem>
+                <MenuItem value={20}>오프라인</MenuItem>
               </Select>
             </FormControl>
             <FormControl
@@ -94,11 +176,15 @@ const WritePage = () => {
                 width: { md: "100%", lg: "47.9%" },
               }}
             >
-              <InputLabel>Age</InputLabel>
-              <Select value={age} label="Age" onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+              <InputLabel>진행기간</InputLabel>
+              <Select
+                value={expected_term}
+                label="form_term"
+                onChange={termHandler}
+              >
+                <MenuItem value={10}>기간 미정</MenuItem>
+                <MenuItem value={20}>단기</MenuItem>
+                <MenuItem value={30}>장기</MenuItem>
               </Select>
             </FormControl>
             <FormControl
@@ -108,15 +194,16 @@ const WritePage = () => {
                 width: { md: "100%", lg: "47.9%" },
               }}
             >
-              <InputLabel>Age</InputLabel>
-              <Select value={age} label="Age" onChange={handleChange}>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+              <InputLabel>기술스택</InputLabel>
+              <Select value={stacklist} label="Age" onChange={stacklistHandler}>
+                <MenuItem value={10}>Java</MenuItem>
+                <MenuItem value={20}>Python</MenuItem>
+                <MenuItem value={30}>C</MenuItem>
               </Select>
             </FormControl>
+
             <FormControl sx={{ margin: 1, width: { md: "100%", lg: "47.9%" } }}>
-              <InputLabel>Date</InputLabel>
+              <InputLabel>시작예정일</InputLabel>
               <DatePicker
                 selected={startDate}
                 onChange={date => setStartDate(date)}
@@ -151,6 +238,39 @@ const WritePage = () => {
           />
         </Grid>
         <Grid item xs={1}></Grid>
+
+        <Grid
+          item
+          // xs={12}
+          sm={11}
+          container
+          direction="row"
+          justifyContent="flex-end"
+        >
+          <button
+            style={{
+              border: "none",
+              backgroundColor: "#999999",
+              color: "black",
+              float: "right",
+            }}
+            onClick={back}
+            className="buttonComplete"
+            // className={styles.buttonComplete}
+            name="register"
+          >
+            취소
+          </button>
+
+          <button
+            onClick={addPost}
+            className="buttonComplete"
+            // className={styles.buttonComplete}
+            name="register"
+          >
+            글 등록
+          </button>
+        </Grid>
       </Grid>
     </>
   );
