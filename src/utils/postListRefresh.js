e8062db258784776fd0f06654e5d505f95b53fb1
ParkJiYeon <kkll4106@naver.com>
2023-01-18 14:13:47 +0900

    feat : 무한 스크롤 기능 수정

--- src/utils/postListRefresh.js
+++ src/utils/postListRefresh.js
@@ -1,6 +1,6 @@
 export default function postListRefresh(postListShow, postListData, stackList) {
   postListShow = postListData.filter(data =>
-    data.designated_stacks.some(i => stackList.includes(i))
+    data.designatedStacks.some(i => stackList.includes(i))
   );
   if (stackList.length === 0) postListShow = postListShow.concat(postListData);
   return postListShow;
